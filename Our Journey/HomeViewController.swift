48304e0b35a9f250875c6defcd3158dbdbe4808e
austin03 <austin03@ksu.edu>
2018-01-21 19:41:17 -0600

    Enhanced TabBar item icons, added custom controller to TabBarController, and modified look of the Tab Bar

--- Our Journey/HomeViewController.swift
+++ Our Journey/HomeViewController.swift
@@ -6,20 +6,23 @@
 //  Copyright © 2018 Austin Elliott. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class HomeViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		// Setting up custom background
+		UIGraphicsBeginImageContext(self.view.frame.size)
+		UIImage(named: "Background")?.draw(in: self.view.bounds)
+		let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+		UIGraphicsEndImageContext()
+		self.view.backgroundColor = UIColor (patternImage: image)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
-
--- Our Journey/MainTabController.swift
+++ Our Journey/MainTabController.swift
@@ -1,28 +1,30 @@
 //
-//  FifthController.swift
+//  MainTabController.swift
 //  Our Journey
 //
-//  Created by Austin Elliott on 1/7/18.
+//  Created by Austin Elliott on 1/21/18.
 //  Copyright © 2018 Austin Elliott. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class ComingSoonViewController: UIViewController {
+class MainTabController : UITabBarController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Setting up custom background
+		
 		UIGraphicsBeginImageContext(self.view.frame.size)
 		UIImage(named: "Background")?.draw(in: self.view.bounds)
 		let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
 		UIGraphicsEndImageContext()
 		self.view.backgroundColor = UIColor(patternImage: image)
-	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+		
+		let blur = UIBlurEffect(style: UIBlurEffectStyle.dark)
+		let blurView = UIVisualEffectView(effect: blur)
+		blurView.frame = self.tabBar.bounds
+		blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		self.tabBar.addSubview(blurView)
+		self.tabBar.sendSubview(toBack: blurView)
 	}
 }
